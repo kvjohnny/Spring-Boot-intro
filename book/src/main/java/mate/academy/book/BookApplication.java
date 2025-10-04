1551eb0470e1da3d0d74d955ad818dbe005b0ec5
kvjohnny <kvjohnny.van@gmail.com>
2025-10-05 00:06:04 +0300

    fixed lombok plugin in pom.xml file

---
+++ book/src/main/java/mate/academy/book/BookApplication.java
@@ -0,0 +1,41 @@
+package mate.academy.book;
+
+import mate.academy.book.model.Book;
+import mate.academy.book.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.math.BigDecimal;
+
+@SpringBootApplication
+public class BookApplication {
+    @Autowired
+    private BookService bookService;
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner() {
+        return new CommandLineRunner() {
+            @Override
+            public void run(String... args) throws Exception {
+                Book book = new Book();
+                book.setTitle("Love story");
+                book.setAuthor("Erich Segal");
+                book.setIsbn("9783596167692");
+                book.setPrice(BigDecimal.valueOf(350));
+                book.setDescription("Book about love story");
+                book.setCoverImage("Love story cover");
+
+                bookService.save(book);
+
+                System.out.println(bookService.findAll());
+            }
+        };
+    }
+}
