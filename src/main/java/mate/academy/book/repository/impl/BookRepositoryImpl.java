3ae22a66a3d66471f03c62eb838bc53104925197
kvjohnny <kvjohnny.van@gmail.com>
2025-10-07 21:17:29 +0300

    fixed files after first review

--- src/main/java/mate/academy/book/repository/impl/BookRepositoryImpl.java
+++ src/main/java/mate/academy/book/repository/impl/BookRepositoryImpl.java
@@ -8,9 +8,9 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 
-@Component
+@Repository
 public class BookRepositoryImpl implements BookRepository {
     private final SessionFactory sessionFactory;
 
--- src/main/java/mate/academy/book/service/impl/BookServiceImpl.java
+++ src/main/java/mate/academy/book/service/impl/BookServiceImpl.java
@@ -1,16 +1,16 @@
 package mate.academy.book.service.impl;
 
 import java.util.List;
+import lombok.RequiredArgsConstructor;
 import mate.academy.book.model.Book;
 import mate.academy.book.repository.BookRepository;
 import mate.academy.book.service.BookService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class BookServiceImpl implements BookService {
-    @Autowired
-    private BookRepository bookRepository;
+    private final BookRepository bookRepository;
 
     @Override
     public Book save(Book book) {
