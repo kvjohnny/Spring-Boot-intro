309ea9be3a66a0b1d99f533def94d96f00e4583d
kvjohnny <kvjohnny.van@gmail.com>
2025-10-09 22:13:04 +0300

    fixed .gitignore and Book.java files

--- src/main/java/mate/academy/book/model/Book.java
+++ src/main/java/mate/academy/book/model/Book.java
@@ -9,10 +9,12 @@ import jakarta.persistence.Table;
 import java.math.BigDecimal;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 @Entity
 @Getter
 @Setter
+@ToString
 @Table(name = "books")
 public class Book {
     @Id
