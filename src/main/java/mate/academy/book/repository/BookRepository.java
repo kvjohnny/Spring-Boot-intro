437e0785f3a9a94dc59c7b8cabce848fa76623fb
kvjohnny <kvjohnny.van@gmail.com>
2025-10-29 20:13:57 +0200

    fixed files in ability-to-search-books-hw-solution task

--- src/main/java/mate/academy/book/repository/BookRepository.java
+++
@@ -1,7 +0,0 @@
-package mate.academy.book.repository;
-
-import mate.academy.book.model.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookRepository extends JpaRepository<Book, Long> {
-}
--- src/main/java/mate/academy/book/service/impl/BookServiceImpl.java
+++ src/main/java/mate/academy/book/service/impl/BookServiceImpl.java
@@ -1,14 +1,19 @@
 package mate.academy.book.service.impl;
 
 import java.util.List;
+import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import mate.academy.book.dto.BookDto;
+import mate.academy.book.dto.BookSearchParametersDto;
 import mate.academy.book.dto.CreateBookRequestDto;
 import mate.academy.book.exception.EntityNotFoundException;
 import mate.academy.book.mapper.BookMapper;
 import mate.academy.book.model.Book;
-import mate.academy.book.repository.BookRepository;
+import mate.academy.book.repository.book.BookRepository;
+import mate.academy.book.repository.book.BookSearchParametersMapper;
+import mate.academy.book.repository.book.SpecificationProvider;
 import mate.academy.book.service.BookService;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -16,6 +21,8 @@ import org.springframework.stereotype.Service;
 public class BookServiceImpl implements BookService {
     private final BookRepository bookRepository;
     private final BookMapper bookMapper;
+    private final SpecificationProvider<Book> bookSpecificationProvider;
+    private final BookSearchParametersMapper bookSearchParametersMapper;
 
     @Override
     public BookDto save(CreateBookRequestDto requestDto) {
@@ -49,4 +56,24 @@ public class BookServiceImpl implements BookService {
         bookMapper.toUpdatedModel(book, requestDto);
         return bookMapper.toDto(bookRepository.save(book));
     }
+
+    @Override
+    public List<BookDto> search(Map<String, String> param) {
+        Specification<Book> specification = Specification.not(null);
+        for (Map.Entry<String, String> entry : param.entrySet()) {
+            Specification<Book> sp = bookSpecificationProvider
+                    .getSpecification(entry.getKey(), entry.getValue());
+            specification = specification.and(sp);
+        }
+        return bookRepository.findAll(specification).stream()
+                .map(bookMapper::toDto)
+                .toList();
+    }
+
+    @Override
+    public List<BookDto> search(BookSearchParametersDto searchParameters) {
+        Map<String, String> param = bookSearchParametersMapper.toMap(searchParameters);
+        return search(param);
+    }
+
 }
