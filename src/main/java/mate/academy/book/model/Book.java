1b8c4336402de9cb740a618f153cce32bc5368c2
kvjohnny <kvjohnny.van@gmail.com>
2025-11-29 15:23:54 +0200

    fixed files in add-category-model-solution task

--- src/main/java/mate/academy/book/model/Book.java
+++ src/main/java/mate/academy/book/model/Book.java
@@ -43,7 +43,7 @@ public class Book {
     private boolean isDeleted = false;
     @ManyToMany
     @JoinTable(
-            name = "book_category",
+            name = "books_categories",
             joinColumns = @JoinColumn(name = "book_id"),
             inverseJoinColumns = @JoinColumn(name = "category_id")
     )
